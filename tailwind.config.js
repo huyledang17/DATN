eb4888a1f400d514a306826743d3cc8e0e5f5bb1
huyledang17 <ledanghuy200098@gmail.com>
2023-12-04 18:54:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
+  ],
+  darkMode: ['class', '[data-theme="dark"]'],
+
+  theme: {
+    extend: {
+      colors: {
+        'main-red': '#eb7260',
+        'main-black': '#354458',
+      },
+      width: {
+        container: '1300px',
+        '380px': '380px',
+      },
+      height: {
+        header: '80px',
+      },
+      margin: {
+        '380px': '380px',
+      },
+      fontSize: {
+        '35px': '35px'
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
+};
